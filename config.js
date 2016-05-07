13df3017a93300d70a0cdc1d22859c171696ec97
gaoyangxiaozhu <gyyzyp@163.com>
2016-05-07 18:29:59 +0800

    modify config.js set dbname can be config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.mysql = {
     host : '0.0.0.0',
     port : '3333',
     user : 'root',
-    password : '123123',
-    database : 'docker'
+    password : '123123'
 };
+exports.dbname = 'docker';
 exports.swarm_add = "http://10.103.242.128:2377/"
