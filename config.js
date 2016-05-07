7d46914f268722e41e9fe88cbd58ea25a84dc897
gaoyangxiaozhu <gyyzyp@163.com>
2016-05-07 12:23:45 +0800

    modify config.js

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 exports.mysql = {
-    host: '0.0.0.0',
-    post: '3333',
-}
+    host : '0.0.0.0',
+    port : '3333',
+    user : 'root',
+    password : '123123',
+    database : 'docker'
+};
+exports.swarm_add = "http://10.103.242.128:2377/"
